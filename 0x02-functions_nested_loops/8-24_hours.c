a808e90919fda48a6b9c61d78d8ae4d1a1d6b160
sammiesuama <samuelisuama95@gmail.com>
2022-10-20 11:46:45 -0700

    8-24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,12 +10,12 @@ int a = 0, b = 0, c = 0, d = 0;
 while (x < 1440)
 {
 _putchar(a + '0');
-_puchar(b + '0');
+_putchar(b + '0');
 _putchar(':');
 _putchar(c + '0');
 _putchar(d + '0');
 _putchar('\n');
-d++
+d++;
 if (d > 9)
 {
 d = 0;
