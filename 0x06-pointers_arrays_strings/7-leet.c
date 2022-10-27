9ec6c4ea196761ea0cfbe2de5691b0892a3dd238
sammiesuama <samuelisuama95@gmail.com>
2022-10-27 13:33:10 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,7 +11,8 @@ char *leet(char *c)
 char a[] = "aeotlAEOTL";
 char b[] = {'4', '3', '0', '7', '1', '4', '3', '0', '7', '1'};
 int x, y;
-for (x = 0; c[x] != '\0'; y++)
+for (x = 0; c[x] != '\0'; x++)
+for (y = 0; a[y] != '\0'; y++)
 if (c[x] == a[y])
 c[x] = b[y];
 return (c);
