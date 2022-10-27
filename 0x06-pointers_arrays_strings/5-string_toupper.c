5669fc5c0a0cea46d198651b42e4c22b11829e1a
sammiesuama <samuelisuama95@gmail.com>
2022-10-27 12:22:31 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *string_touper - change all lowercase letters of a string to uppercase
+ *
+ *@p: pointer to char to check
+ *
+ *Return: 0 is success
+ */
+char *string_touper(char *p)
+{
+int a;
+for (a = 0; p[a] != '\0'; a++)
+{
+if (p[a] >= 97 && p[a] <= 122)
+p[a] -= 32;
+}
+return (p);
+}
